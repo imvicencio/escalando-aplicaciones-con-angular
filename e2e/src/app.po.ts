7d943a2c4b6cba53100b8ad6973e6d1be171f2ae
Ismael Vicencio <imvicencio@live.cl>
2018-12-12 12:39:18 -0300

    creacion de pruba con query selector all

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -2,11 +2,16 @@ import { browser, by, element } from 'protractor';
 
 export class AppPage {
   selectors = {
-    'title' : 'app-root h1'
+    'title' : 'app-root h1',
+    'card-titles' : 'mat-card-title'
   };
 
   navigateTo() {
     return browser.get('/');
+  }
+
+  getCardTitles() {
+    return element.all(by.css(this.selectors['card-titles']));
   }
 
   getTitleText() {
